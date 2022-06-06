073aa144298d46209f2a5381a97bd6423e9e1fae
Jetniphan Pukkham <jeni8399@gmail.com>
2022-06-07 00:17:08 +0700

    first

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHero.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero {
+                    Id = 1,
+                    Name = "Spider Man",
+                    FirstName="Peter",
+                    LastName="Parker",
+                    Place="New York City"
+                }
+             };
+            return NotFound(heroes);
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperHero.Data
+{
+    public class DataContext : DbContext
+	{
+		public DataContext()
+		{
+		}
+	}
+}
+
